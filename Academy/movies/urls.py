6d59dd25788ce9983e67ab2b64a097f05609a4b8
bozdag05 <muhammadosmanov02@gmail.com>
2023-10-23 12:40:09 +0300

    add pagination | func search

--- Academy/movies/urls.py
+++ Academy/movies/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.MoviesView.as_view()),
+    path("filter/", views.FilterMovieView.as_view(), name="filter"),
+    path("search/", views.Search.as_view(), name="search"),
     path("<slug:slug>/", views.MovieDetailView.as_view(), name='movie_detail'),
     path("review/<int:pk>/", views.AddReview.as_view(), name='add_review'),
     path("actor/<str:slug>", views.ActorDetailView.as_view(), name='actor_detail')
--- Academy/movies/views.py
+++ Academy/movies/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
-
+from django.db.models import Q
 from .models import Movie, Type_movie, Actor, Country, Category
 from .forms import ReviewForm
 
@@ -18,17 +18,12 @@ class GenreCategoryCountry:
 
 class MoviesView(GenreCategoryCountry, ListView):
     model = Movie
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['movies'] = Movie.objects.filter(is_published=True)
-
-        return context
-
     template_name = 'movies/movies.html'
+    paginate_by = 2
+    context_object_name = 'movies'
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreCategoryCountry, DetailView):
     model = Movie
     slug_field = 'url'
     template_name = 'movies/moviedetail.html'
@@ -53,3 +48,36 @@ class ActorDetailView(DetailView):
     slug_field = 'name'
 
 
+class FilterMovieView(GenreCategoryCountry, ListView):
+
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(country__in=self.request.GET.getlist("country")) |
+            Q(category__in=self.request.GET.getlist("category"))
+        ).distinct()
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["country"] = ''.join([f"country={x}&" for x in self.request.GET.getlist("country")])
+        context["category"] = ''.join([f"category={x}&" for x in self.request.GET.getlist("category")])
+        return context
+
+    context_object_name = 'movies'
+    template_name = "movies/movies.html"
+
+
+class Search(ListView):
+    context_object_name = 'movies'
+    template_name = "movies/movies.html"
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Movie.objects.filter(title__icontains=self.request.GET.get("q"))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["q"] = f'q={self.request.GET.get("q")}'
+        return context
